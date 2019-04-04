0bc5570b36cb4e0ed126e40abe1f24f156cfa7a6
clearsky <38609798+clearsky@users.noreply.github.com>
2019-04-04 10:44:25 +0800

    update logging

--- ShortUrl/settings.py
+++ ShortUrl/settings.py
@@ -138,7 +138,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'standard': {
-            'format': '[asctime][{threadName}:{thread}][task_id:{name}][{filename}:{lineno}]'
+            'format': '[{asctime}][{threadName}:{thread}][task_id:{name}][{filename}:{lineno}]'
                       '[{levelname}][{message}]',
             'style': '{',
         },
